d05d38e00b6e0044ffe2903a303230f21a0821b6
Edgar Nazyrov <edgar.nazyrov@finch-xr.com>
2022-03-01 21:12:32 +0500

    create base AnimeAPI

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ShikiSwiftApi",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,14 +15,14 @@ let package = Package(
             targets: ["ShikiSwiftApi"]),
     ],
     dependencies: [
-        .package(name: "OAuth2", url: "https://github.com/p2/OAuth2", branch: "master")
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ShikiSwiftApi",
-            dependencies: []),
+            dependencies: ["Alamofire"]),
         
         .testTarget(
             name: "ShikiSwiftApiTests",
